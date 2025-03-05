792f5f610961d3647840037bdc3d08fa9c1bd113
alonbeck <alon@thepanax.com>
2025-03-05 16:55:46 +0200

    please make this one the last

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ let notes = [
   }
 ]
 
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
